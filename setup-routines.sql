ee8750e317cde68b5bb87aa6e7efa0efa119ec2a
sreemanti-dey <56366201+sreemanti-dey@users.noreply.github.com>
2024-03-11 20:22:44 -0700

    Update setup-routines.sql

--- setup-routines.sql
+++ setup-routines.sql
@@ -1,4 +1,39 @@
+DROP FUNCTION top_restaurant_loc;
 DROP PROCEDURE sp_find_chains;
+DROP TRIGGER trg_new_rating;
+
+
+-- UDF
+
+DELIMITER !
+
+-- Get the top rated restaurant in a given location
+CREATE FUNCTION top_restaurant_loc (loc VARCHAR(200)) 
+                                    RETURNS VARCHAR(50) DETERMINISTIC
+BEGIN
+    DECLARE highest_rated_restaurant VARCHAR(200);
+    DECLARE highest_avg_rating NUMERIC(2, 1);
+
+    SELECT restaurant_name, MAX(avg_rating) AS max_avg_rating
+    INTO highest_rated_restaurant, 
+    FROM (SELECT restaurant_id, AVG(rating) AS avg_rating
+          FROM rating LEFT JOIN user_rating
+          GROUP BY restaurant_id) AS rest_ratings
+          LEFT JOIN restaurant
+    WHERE restaurant_loc = loc;
+
+    RETURN highest_rated_restuarant;
+
+END !
+-- Back to the standard SQL delimiter
+DELIMITER ;
+
+
+
+
+
+
+
 
 DELIMITER !
 CREATE PROCEDURE sp_find_chains(
@@ -42,4 +77,81 @@ BEGIN
 
 END !
 
+DELIMITER ;
+
+
+
+
+
+
+
+
+-- Trigger with procedure on materialized view
+-- Have an MV of the highest average rating restaurants by cuisine
+-- Will update this MV every time a new rating is added
+
+CREATE TABLE mv_top_restaurants_by_cuisine (
+    cuisine_id 	         INTEGER         NOT NULL,
+    restaurant_id        INTEGER         NOT NULL,
+    avg_rating           NUMERIC(2, 1)   NOT NULL,
+    PRIMARY KEY (cuisine_name, restuarant_name),
+    FOREIGN KEY (cuisine_id)
+        REFERENCES cuisine(cuisine_id),
+    FOREIGN KEY (restaurant_id)
+        REFERENCES restaurant(restaurant_id)
+);
+
+INSERT INTO mv_top_restaurants_by_cuisine (cuisine_id,
+                                           restaurant_id,
+                                           avg_rating)
+SELECT cuisine_id,
+       restaurant_id,
+       AVG(rating) AS avg_rating
+FROM rating LEFT JOIN user_rating LEFT JOIN in_cuisine
+GROUP BY cuisine_id, restaurant_id
+HAVING avg_rating >= 8.0
+ORDER BY cuisine_id ASC, avg_rating DESC;
+
+
+DELIMITER !
+
+CREATE PROCEDURE sp_cuisinetoprest_newrating(
+    new_restaurant_id INTEGER,
+    new_rating NUMERIC(2, 1)
+)
+BEGIN 
+    CREATE TABLE new_mv (
+        cuisine_id 	         INTEGER         NOT NULL,
+        restaurant_id        INTEGER         NOT NULL,
+        avg_rating           NUMERIC(2, 1)   NOT NULL,
+        PRIMARY KEY (cuisine_name, restuarant_name),
+        FOREIGN KEY (cuisine_id)
+            REFERENCES cuisine(cuisine_id),
+        FOREIGN KEY (restaurant_id)
+            REFERENCES restaurant(restaurant_id)
+    );
+
+    INSERT INTO new_mv (cuisine_id,
+                        restaurant_id,
+                        avg_rating)
+    SELECT cuisine_id,
+        restaurant_id,
+        AVG(rating) AS avg_rating
+    FROM rating LEFT JOIN user_rating LEFT JOIN in_cuisine
+    GROUP BY cuisine_id, restaurant_id
+    HAVING avg_rating >= 8.0
+    ORDER BY cuisine_id ASC, avg_rating DESC;
+
+    DROP TABLE mv_top_restaurants_by_cuisine;
+
+    RENAME TABLE new_mv TO mv_top_restaurants_by_cuisine;
+
+END !
+
+-- Handles new rows added to rating table, updates MV accordingly
+CREATE TRIGGER trg_new_rating AFTER INSERT
+       ON rating FOR EACH ROW
+BEGIN
+    CALL sp_highest_(OLD.branch_name, OLD.balance);
+END !
 DELIMITER ;
