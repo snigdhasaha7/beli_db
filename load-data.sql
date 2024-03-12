57a1538e263cd2cb001301c1af7b2baed02a5ad4
sreemanti-dey <56366201+sreemanti-dey@users.noreply.github.com>
2024-03-11 22:34:16 -0700

    Add files via upload
    added load file

---
+++ load-data.sql
@@ -0,0 +1,42 @@
+-- Instructions:
+-- This script will load the CSV files generated for this FP
+-- into the tables created in setup.sql.
+-- Intended for use with the command-line MySQL, otherwise unnecessary for
+-- phpMyAdmin (just import each CSV file in the GUI).
+
+-- MUST load in users with pwd management BEFORE trying to run this file.
+-- Might need SET GLOBAL local_infile = 'ON' cmd first
+-- Might need to start mysql with --local-infile=1
+
+LOAD DATA LOCAL INFILE 'data/friends_data.csv' INTO TABLE friend
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/category_data.csv' INTO TABLE category
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/cuisine_data.csv' INTO TABLE cuisine
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/restaurant_data.csv' INTO TABLE restaurant
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/in_category_data.csv' INTO TABLE in_category
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/in_cuisine_data.csv' INTO TABLE in_cuisine
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/rating_data.csv' INTO TABLE rating
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/user_rating_data.csv' INTO TABLE user_rating
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/chain_data.csv' INTO TABLE chain
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/belongs_in_chain_data.csv' 
+INTO TABLE belongs_in_chain
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+
